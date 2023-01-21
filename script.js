b9324f22d99bddb58e0bb9c4130b7ff6e37d3c16
Binatarani <binatarani.bd@gmail.com>
2023-01-21 18:49:21 +0600

    Java Methods

---
+++ script.js
@@ -0,0 +1,53 @@
+// // 1) Array Filter()
+// const age1 = [32, 33, 16, 40, 60, 70, 12, 11];
+// // const products = [{},{}]
+// function isVoter(vAge) {
+//   return vAge >= 18;
+// }
+// const voterList = age1.filter(isVoter);
+// console.log(voterList);
+// console.log(age1);
+
+//2) Array find() one item return
+
+// const age1 = [32, 33, 16, 40, 60, 70, 12, 11];
+
+// function checkPerson(pAge) {
+//   return pAge > 40;
+// }
+// const person = age1.find(checkPerson);
+// console.log(person);
+
+// const age1 = [32, 33, 16, 40, 60, 70, 12, 11];
+
+// function checkPerson(pAge) {
+//   return pAge == 40;
+// }
+// const person = age1.find(checkPerson);
+// console.log(person);
+
+// // 3) Array map()
+
+// const num = [32, 33, 16, 40, 60, 70, 12, 11];
+
+// function newFunction(n) {
+//   return n * 2;
+// }
+// const newNumber = num.map(newFunction);
+// console.log(newNumber);
+
+// // 4) Array reduct()
+
+// const num = [32, 33, 16, 40, 60, 70, 12, 11];
+
+// function getTotal(first, next) {
+//   return first + next;
+// }
+// const total = num.reduce(getTotal);
+// console.log(total);
+
+// // Array Sort()
+
+// const fruits = ["Banana", "Apple", "Mango", "Orange"];
+// fruits.sort();
+// console.log(fruits);
